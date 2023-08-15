6ac0aa3dc0afbd6fc158520f7fd98c94af6585a3
Huda7777777 <hudahuda77777777@gmail.com>
2023-08-15 11:22:24 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- *  program that prints _putchar
- *  Return always 0
+ *  main - Entry point
+ *  Description: 'prints _putchar to stdout'
+ *  Return: always 0
  */
 int main(void)
 {
