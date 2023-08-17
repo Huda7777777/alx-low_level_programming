a866c1c457a11e2b368bd6309109e8369d846aa6
Huda7777777 <hudahuda77777777@gmail.com>
2023-08-17 11:19:25 -0700

    4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,8 +5,8 @@
  *
  * Return: void
  */
- 
-  void print_most_numbers(void)
+
+void print_most_numbers(void)
 {
 	int num;
 	for (num = 0; num <= 9; num++)
@@ -16,4 +16,3 @@
 	}
 	_putchar('\n');
 }
- 
