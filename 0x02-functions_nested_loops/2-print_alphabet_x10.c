63bcb2362b933320027e329138cf8bd54d50f446
Huda7777777 <hudahuda77777777@gmail.com>
2023-08-24 03:52:57 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,12 +6,13 @@
 void print_alphabet_x10(void)
 {
 	char letter;
-	int count =0:
+	int count = 0;
 
 		while (count++ <= 9)
 		{
 			for (letter = 'a' ; letter <= 'z'; letter++)
-				_putchar(letter):
-					_putchar('\n'):
+				_putchar(letter);
+					_putchar('\n');
 		}
 }
+
