c89f9a752c6091fd820d8b3ae6ae3e12f2e73d5c
Huda7777777 <hudahuda77777777@gmail.com>
2023-08-24 03:13:33 -0700

    1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * -strncat - concatenates two strings
+ *
+ *  @desr: 1st param
+ *  @src: 2nd param
+ *  @n: 3rd param
+ *  Return: string
+ */
+
+char *_strncat(char *dest, char *src, int n);
+{
+	strncat(dest, src, n);
+		return (dest);
+}
