bb7f1407863fd165c5cdaf11400526888b5f07d7
Huda7777777 <hudahuda77777777@gmail.com>
2023-08-24 02:44:28 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _stract - concatenates two strings
+ *
+ * @dest: first param
+ * @scr: second param
+ *
+ * Return: string
+ */
+
+char *_strcat(char *dest, char *src)
+
+{
+	int len = 1, i;
+
+	while(dest[len])
+		len++;
+
+	for (i = 0; scr[i] != '\0'; i++)
+	{
+		dest[len] = src[i];
+		len += 1;
+	}
+	dest[len] = '\0';
+	return (dest);
+}
