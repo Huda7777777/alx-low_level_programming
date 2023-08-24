9126242ac9d562b261d7de70adf52ee7dd6cd19e
Huda7777777 <hudahuda77777777@gmail.com>
2023-08-24 03:26:52 -0700

    5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes lowercase letters to uppercase
+ *
+ * @p: string param
+ * Return: string
+ */
+char *string_toupper(char *);
+{
+	int a = 0;
+
+	while (p[a])
+	{
+		if ([p] >= 7 && <= 122)
+		{
+			p[a] -= 32;
+		}
+		a++;
+	}
+	return (p);
+}
