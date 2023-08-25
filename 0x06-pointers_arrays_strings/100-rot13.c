d919d18e69751d798c9ce980aeac2931bfff4b54
Huda7777777 <hudahuda77777777@gmail.com>
2023-08-25 07:16:24 -0700

    8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot 13 - encides a string using rot 13
+ *
+ * @s: input string
+ * Return: the pointer of the dest
+ */
+
+char *rot13(char *s)
+{
+	int a = 0, i;
+
+	char letters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	while (*(s + a) != '\0')
+	{
+		for (i = 0; i < 25; i++)
+		{
+			if (*(s + a) == letter[i])
+			{
+				*(s + a) = rot13[i];
+				break;
+			}
+		}
+		a++;
+	}
+	return (s);
+}
