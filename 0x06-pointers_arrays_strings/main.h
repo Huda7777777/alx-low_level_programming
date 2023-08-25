c845d57a881661b2192fcd528ac39af69c1e5b82
Huda7777777 <hudahuda77777777@gmail.com>
2023-08-25 13:06:53 -0700

    done

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -14,5 +14,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_number(int n);
 char *rot13(char *);
 char *_strcat(char *dest, char *src);
+void print_number(int n);
+int _putchar(char c);
 
 #endif
