8e0970fb2234cd0142c198cbf2da34b3420b08f1
Huda7777777 <hudahuda77777777@gmail.com>
2023-08-27 02:36:22 -0700

    done

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncpy - function that copies a string 
+ *
+ * @dest: param1
+ * @n: param2
+ * @src: param3
+ * Return: string
+ */
+char *_strncp(char *dest, char *src, int n)
+{
+	strncp(dest, src, n);
+	return(dest);
+}
